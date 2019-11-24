80424febbd6fdf816b7c4fe4770cd35be794e454
Andrew Wooldridge <triptych@gmail.com>
2019-11-23 22:22:29 -0800

    playing with states

--- App.svelte
+++ App.svelte
@@ -1,5 +1,18 @@
 <script>
-	import Button from "./Button.svelte";
+  import Button from "./Button.svelte";
+  let state = {
+    page: "two"
+  };
+
+  function show() {
+    state.page = "one";
+    console.log("one");
+  }
+
+  function hide() {
+    state.page = "two";
+    console.log("two");
+  }
 </script>
 
 <style>
@@ -13,5 +26,14 @@
 	<h1>Hello CodeSandbox</h1>
 	<h2>Start editing to see some magic happen!</h2>
 	<Button />
+<hr/>
+
+{#if state.page =='two'}
+  <button on:click={show}>show</button>
+  {/if}
+{#if state.page=='one'}  
+  <button on:click={hide}>hide</button>
+{/if}
+
 </main>
 
